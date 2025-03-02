de1a1175d93966dd0b3b88d36d3a1174c530b2b8
PhabloY <yuripw3p@outlook.com>
2025-03-02 17:21:33 -0300

    display

--- main.py
+++ main.py
@@ -1,21 +1,26 @@
 import sys
-from PySide6.QtWidgets import (QApplication, QMainWindow, QWidget, QVBoxLayout,
-                               QLabel,)
+from PySide6.QtWidgets import (QApplication)
+from display import Display
 from main_window import MainWindow
 from PySide6.QtGui import QIcon
 from variables import WINDOW_ICON_PATH
 
 
 if __name__ == '__main__':
+    # cria a aplicação
     app = QApplication(sys.argv)
     window = MainWindow()
-    label1 = QLabel('O meu texto')
-    label1.setStyleSheet('font-size: 40px;')
-    window.addWidgetToVLayout(label1)
-    window.adjustfixedsize()
+
+    # define o icone
     icon = QIcon(str(WINDOW_ICON_PATH))
     window.setWindowIcon(icon)
-    window.adjustfixedsize()
+    app.setWindowIcon(icon)
 
+    # Display
+    display = Display()
+    window.addToVLayout(display)
+
+    # executa tudo
+    window.adjustfixedsize()
     window.show()
     app.exec()
--- variables.py
+++ variables.py
@@ -3,3 +3,11 @@ from pathlib import Path
 ROOT_DIR = Path(__file__).parent
 FILES_DIR = ROOT_DIR / 'files'
 WINDOW_ICON_PATH = FILES_DIR / '2927547.png'
+
+# sizing
+
+BIG_FONT_SIZE = 20
+MEDIUM_FONT_SIZE = 16
+SMALL_FONT_SIZE = 12
+TEXT_MARGIN = 10
+MINIMUM_WIDTH = 600
