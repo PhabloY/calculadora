fc37b1c58e2d7c6da75f263c5230885e9ee21f81
PhabloY <yuripw3p@outlook.com>
2025-03-03 16:21:44 -0300

    theme

--- main.py
+++ main.py
@@ -1,20 +1,27 @@
 import sys
 from PySide6.QtWidgets import (QApplication)
+from info import Info
 from display import Display
 from main_window import MainWindow
 from PySide6.QtGui import QIcon
 from variables import WINDOW_ICON_PATH
+from styles import setupTheme
 
 
 if __name__ == '__main__':
     # cria a aplicação
     app = QApplication(sys.argv)
+    setupTheme(app)
     window = MainWindow()
 
     # define o icone
     icon = QIcon(str(WINDOW_ICON_PATH))
     window.setWindowIcon(icon)
     app.setWindowIcon(icon)
+
+    # info
+    info = Info('2.0 ^ 10.0 = 1024')
+    window.addToVLayout(info)
 
     # Display
     display = Display()
