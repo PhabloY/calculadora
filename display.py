bacb759eb24da0b46591e6f287a4fa7d1f2e582c
PhabloY <yuripw3p@outlook.com>
2025-03-13 12:52:23 -0300

    update

--- display.py
+++ display.py
@@ -2,10 +2,13 @@ from PySide6.QtGui import QKeyEvent
 from PySide6.QtWidgets import QLineEdit
 from variables import BIG_FONT_SIZE, TEXT_MARGIN, MINIMUM_WIDTH
 from PySide6.QtCore import Qt, Signal
+from utils import isEmpty
 
 
 class Display(QLineEdit):
-    eqRequested = Signal()
+    eqPressed = Signal()
+    delPressed = Signal()
+    clearPressed = Signal()
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
@@ -20,11 +23,30 @@ class Display(QLineEdit):
         self.setTextMargins(*margins)
 
     def keyPressEvent(self, event: QKeyEvent) -> None:
+        text = event.text().strip()
         key = event.key()
         KEYS = Qt.Key
 
         isEnter = key in [KEYS.Key_Enter, KEYS.Key_Return]
+        isDelete = key in [KEYS.Key_Backspace, KEYS.Key_Delete]
+        isEsc = key in [KEYS.Key_Escape]
 
         if isEnter:
-            self.eqRequested.emit()
+            print('Enter pressionado, sinal emitido', type(self).__name__)
+            self.eqPressed.emit()
             return event.ignore()
+
+        if isDelete:
+            print('isDelete pressionado, sinal emitido', type(self).__name__)
+            self.delPressed.emit()
+            return event.ignore()
+
+        if isEsc:
+            print('isEsc pressionado, sinal emitido', type(self).__name__)
+            self.clearPressed.emit()
+            return event.ignore()
+
+        if isEmpty(text):
+            return event.ignore()
+
+        print('Texto', text)
